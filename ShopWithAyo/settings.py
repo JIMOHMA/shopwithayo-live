2d802018c45e745bba59a75be48e325ddb406d91
ayodele <techwithayodele@outlook.com>
2021-02-18 04:27:19 -0500

    App Password added for google

--- ShopWithAyo/settings.py
+++ ShopWithAyo/settings.py
@@ -28,7 +28,7 @@ DEBUG = False
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'mail4ayodele@gmail.com'
-EMAIL_HOST_PASSWORD = 'delegiwa24.'
+EMAIL_HOST_PASSWORD = 'uheuhqbugdvsozpb'
 EMAIL_PORT = 587
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
